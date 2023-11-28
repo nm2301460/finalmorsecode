6feb601239ad5289dd01eb74366160633838e2c8
[Noura Rabie] <[NM2301460@tkh.edu.eg]>
2023-11-28 09:02:03 +0200

    adding the encrypt function
    this function is to take the text and change it all to upper case then take each character and find its morse if it was a space put the / instead other raise the error and finally return the result

--- morsecode.py
+++ morsecode.py
@@ -15,3 +15,15 @@ MORSECODE = {
     ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-',
     '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
 }
+# Making a function to turn from text to morse
+def text_to_morse(text):
+    text = text.upper()  # converting the text into upper case
+    result = []
+    for char in text:
+        if char in MORSECODE:
+            result.append(MORSECODE[char])  # Append Morse code for the character
+        elif char == " ":
+            result.append("/")  # Append space for space in the input
+        else:
+            raise MorseError(f"The '{char}' key doesn't exist in the dictionary")  # Raise an error if the character is not in the dictionary
+    return ' '.join(result)  # Join Morse code representations with spaces between characters
